b93e611bdf5f4c2ab6fcacf01a1f77db494778c7
Casper <casper.aakviststeengaard@gmail.com>
2022-03-30 11:50:55 +0200

    Almost done with page 4, need final delete features

--- errorhandlingClient/configuration/fetchUtils.js
+++ errorhandlingClient/configuration/fetchUtils.js
@@ -2,8 +2,24 @@ export async function handleHttpErrors(res) {
     if (!res.ok) {
         const errorResponse = await res.json();
         const error = new Error(errorResponse.message)
+        // @ts-ignore
         error.apiError = errorResponse
         throw error
     }
     return res.json()
+  }
+
+  export function makeOptions(method, body) {
+    const opts = {
+      method: method, 
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    }
+
+    if (body) {
+      opts.body = JSON.stringify(body);
+    }
+    return opts;
   }
